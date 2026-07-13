6363aa46880550b6abf21c98aab3346e7c4291f1
nain-fath <fathimakaunain088@gmail.com>
2026-07-14 01:35:13 +0530

    Add automatic screenshots on test failure

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 import pytest
 from utilities.driver_factory import get_driver
 
@@ -7,3 +10,28 @@ def driver():
     driver = get_driver()
     yield driver
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+
+        driver = item.funcargs.get("driver")
+
+        if driver:
+
+            screenshot_dir = "screenshots"
+
+            os.makedirs(screenshot_dir, exist_ok=True)
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+            file_name = f"{item.name}_{timestamp}.png"
+
+            driver.save_screenshot(
+                os.path.join(screenshot_dir, file_name)
+            )
